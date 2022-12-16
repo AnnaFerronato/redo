ac2ba0f37352a0044120136fe84c880cda6aaf22
andregiachini <andrelucas_giachini@hotmail.com>
2022-12-16 09:47:09 -0300

    update

--- functions.py
+++ functions.py
@@ -6,5 +6,15 @@ def connect_database():
     try:
         conn = psycopg2.connect(
             database = "log",
-            user = "postgres"
+            user = "postgres",
+            password = "4108357923",
+            host = "127.0.0.1",
+            port = "5432"
         )
+
+        return conn
+    except psycopg2.DatabaseError as error:
+        print(error)
+        exit()
+
+
