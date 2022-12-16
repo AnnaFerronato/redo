c7f8b3c3b873b9217525c6c0ea78fd93f6ac68d5
AnnaFerronato <annacarolina0013@gmail.com>
2022-12-16 10:45:13 -0300

    Update Create_Table

--- functions.py
+++ functions.py
@@ -1,14 +1,12 @@
 import psycopg2
 
-
-
 def connect_database():
     try:
         conn = psycopg2.connect(
             database = "log",
             user = "postgres",
-            password = "4108357923",
-            host = "127.0.0.1",
+            password = "12345",
+            host = "localhost",
             port = "5432"
         )
 
@@ -23,3 +21,32 @@ def close_database(conn):
         conn.close()
 
 
+def create_table():
+    commands = (
+        """drop table if exists dados;""",
+
+        """create table dados(
+            id serial,
+            A integer not null,
+            B integer not null,
+            constraint pk_dados primary key (id)
+        );"""
+    )
+
+    try:
+        conn = connect_database()
+        cur = conn.cursor()
+
+        for command in commands:
+            cur.execute(command)
+        
+        cur.close()
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    close_database(cur)
+
+
+create_table()
